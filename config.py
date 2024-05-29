40e8ec41329a7109c6b15137004cd1d74177f1fc
longyuqing112 <2631918708@qq.com>
2024-05-29 21:44:28 +0800

    sending-file
    Signed-off-by: longyuqing112 <2631918708@qq.com>

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-HOST = "http://192.168.2.20:11001/"
+HOST = "http://127.0.0.1:11001/"
 # address
 DIR_PATH = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
