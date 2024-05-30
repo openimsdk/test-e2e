9a0870562b7d47459265f72b8096671270a004e8
longyuqing112 <2631918708@qq.com>
2024-05-30 12:13:57 +0800

    browser
    Signed-off-by: longyuqing112 <2631918708@qq.com>

--- utils/headless_browser.py
+++ utils/headless_browser.py
@@ -1,12 +1,14 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.options import Options
 
 def create_driver():
-        options = webdriver.ChromeOptions()
+        options = Options()
         options.add_argument('window-size=1920x1080')  # 设置窗口大小匹配Xvfb
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
         # driver = webdriver.Chrome()  # Locally
         # driver.maximize_window()
         driver.implicitly_wait(10)
